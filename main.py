3979023df1080e3adbd0bc7e46e51fddee0b8d9e
Mandeep Singh <mandeeps708@gmail.com>
2016-06-25 22:09:15 +0530

    reduced API calls and some regular expressions to differentiate submodules

--- main.py
+++ main.py
@@ -20,14 +20,18 @@ import re
 class Plugin():
     "Information about plugin."
     # def __init__(self, name, author, plugin_type, description, baseurl, infourl):
-    def __init__(self, name, author, baseurl, description):
+    # def __init__(self, name, author, baseurl, description):
+    def __init__(self, name, baseurl):
         "returns plugin info"
         self.name = name
-        self.author = author
+        # self.author = author
         self.baseurl = baseurl
-        self.description = description
+        # self.description = description
         # self.plugin_type = plugin_type
         # self.infourl = infourl
+
+    def __repr__(self):
+        return 'Plugin(%s)' % (self.name)
 
 class Fetch():
     "The base fetch class"
@@ -58,7 +62,8 @@ class FetchFromGitHub(Fetch):
     def __init__(self):
         print("Fetching GitHub Workbenches")
         # For storing instances of Plugin() class.
-        instances = []
+        # instances = []
+        instances = {}
 
     def getPluginsList(self):
 
@@ -75,58 +80,77 @@ class FetchFromGitHub(Fetch):
             # Name of the repository residing at github_username account.
             repository = "FreeCAD-addons"
 
-            try:
-                # Repository instance.
-                repo = g.get_user(github_username).get_repo(repository)
-                print("Fetching repository details...")
+            # Repository instance.
+            repo = g.get_user(github_username).get_repo(repository)
+            print("Fetching repository details...")
 
-                # To store count of number of submodules.
-                count = 0
+            # To store count of number of submodules.
+            count = 0
 
-                instances = []
+            #instances = []
+            instances = {}
+            
+            # Fetching repository contents.
+            repo_content = repo.get_dir_contents("")
+            print(repo_content)
 
-                # Iterations to fetch submodule entries and their info.
-                for x in repo.get_dir_contents(""):
-                    # if(x.type == "submodule"):
-                        # print(x.url)
+            # Iterations to fetch submodule entries and their info.
+            for x in repo_content:
+		url = str(x.html_url)
+		try:
+		    gitUrl = re.search('(.+?)/tree', url).group(1)
+		except:
+		    pass
+		else:
+		    print(x.name)
+		    print(gitUrl)
+                    instance = Plugin(x.name, gitUrl)
+                    instances[x.name] = instance
+            # ipdb.set_trace()
+            
+            """
+                ######## Some slow code ########
+                #Checks if the instance is a submodule, then fetches it's details.
+                if(x.raw_data.get("type") == "submodule"):
+                    count += 1
+                    submodule_name = x.name
+                    print(submodule_name)
 
-                    #Checks if the instance is a submodule, then fetches it's details.
-                    if(x.raw_data.get("type") == "submodule"):
-                        count += 1
-                        submodule_name = x.name
-                        print(submodule_name)
+                    # URL of submodule repository.
+                    submodule_url = x.raw_data.get("submodule_git_url")
 
-                        # URL of submodule repository.
-                        submodule_url = x.raw_data.get("submodule_git_url")
+                    # Getting the owner name and repository name.
+                    submodule_repoInfo = re.search('https://github.com/(.+?).git', submodule_url).group(1)
+                    
+                    git = Github(token)
+                    # Submodule information.
+                    submodule_repo = git.get_repo(submodule_repoInfo)
+                    submodule_author = submodule_repo.owner.name
+                    submodule_description = submodule_repo.description
 
-                        # Getting the owner name and repository name.
-                        submodule_repoInfo = re.search('https://github.com/(.+?).git', submodule_url).group(1)
+                    # ipdb.set_trace()
 
-                        git = Github(token)
+                    # Creating Plugin class instances.
+                    instance = Plugin(submodule_name, submodule_author, submodule_url, submodule_description)
+                    instances.append(instance)
+                    # print(instances)
 
-                        # Submodule information.
-                        submodule_repo = git.get_repo(submodule_repoInfo)
-                        submodule_author = submodule_repo.owner.name
-                        submodule_description = submodule_repo.description
+                    # ipdb.set_trace()
+                    return instances
+            """
 
-                        # ipdb.set_trace()
-
-                        # Creating Plugin class instances.
-                        instance = Plugin(submodule_name, submodule_author, submodule_url, submodule_description)
-                        instances.append(instance)
-                        # print(instances)
-
-                        # ipdb.set_trace()
-                return instances
-
-            except KeyboardInterrupt:
-                print("\nInterrupted by Keyboard!")
-
-            except:
-                print("Please check your network connection!")
+        except KeyboardInterrupt:
+            print("\nInterrupted by Keyboard!")
 
         except ImportError:
             print("PyGithub isn't installed!")
+
+        #except:
+        #    print("Please check your network connection!")
+
+
+    #def getInfo(PluginName):
+
 
 class FetchFromWiki():
     "fetching macros from wiki"
@@ -165,7 +189,8 @@ class FetchFromWiki():
                     macro_description = soup.select(".macro-description")[0].getText()
                     macro_author = soup.select(".macro-author")[0].getText()
 
-                    macro_instance = Plugin(macro_name, macro_author, macro_url, macro_description)
+                    # macro_instance = Plugin(macro_name, macro_author, macro_url, macro_description)
+                    macro_instance = Plugin(macro_name, macro_url)
                     macro_instances.append(macro_instance)
 
                 return macro_instances
