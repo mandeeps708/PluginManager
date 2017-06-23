f255cf56fd0ff37e70934e781b77df9a5331c7b9
Mandeep Singh <mandeeps708@gmail.com>
2017-06-24 01:31:59 +0530

    UI improved. New layout

--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from PySide import QtGui
 from PySide import QtCore
 from PySide import QtUiTools
@@ -5,26 +6,37 @@ from pluginManager import PluginManager
 
 
 class MyWidget(QtGui.QMainWindow):
+    """PluginManager User Interface."""
     def __init__(self):
         apply(QtGui.QMainWindow.__init__, (self,))
 
+        # Loading Qt Designer UI file.
         loader = QtUiTools.QUiLoader()
-        file = QtCore.QFile("interface.ui")
-        file.open(QtCore.QFile.ReadOnly)
-        self.myWidget = loader.load(file, self)
-        file.close()
+        qt_file = QtCore.QFile("interface.ui")
+        qt_file.open(QtCore.QFile.ReadOnly)
+        self.myWidget = loader.load(qt_file, self)
+        qt_file.close()
 
         self.setCentralWidget(self.myWidget)
-        # QtCore.QObject.connect(self.myWidget.calculateButton, QtCore.SIGNAL("clicked()"), self.accept)
+        # QtCore.QObject.connect(self.myWidget.calculateButton,
+        #                        QtCore.SIGNAL("clicked()"), self.accept)
 
+        # Load plugin list after clicking on "Start". To be automated later.
+        QtCore.QObject.connect(self.myWidget.start_bt,
+                               QtCore.SIGNAL("clicked()"), self.starter)
+
+    def starter(self):
+        """To be initialized after basic UI has loaded."""
+        # Load plugin manager & fetch plugins.
         instance = PluginManager()
         plugins = instance.allPlugins()
         self.accept(plugins)
 
     def accept(self, plugins):
-        data1 = ['a1', 'a2', 'a3', 'a4']
-        data2 = ['b1', 'b2', 'b3', 'b4']
-        # for i in range(len(data1)):
+        """Add plugins to the list widget."""
+        # data1 = ['a1', 'a2', 'a3', 'a4']
+        # data2 = ['b1', 'b2', 'b3', 'b4']
+        # Converting to a list of strings.
         data1 = [str(i) for i in plugins]
         """
         # Using QTableWidget
@@ -35,15 +47,10 @@ class MyWidget(QtGui.QMainWindow):
         # self.myWidget.tablu.setItem(i, 1, item2)
         """
         # Using QListWidget
-        for i in range(len(data1)):
-            self.myWidget.tablu.addItem(data1[i])
-        """
-        v1 = int(self.myWidget.val1.text())
-        v2 = int(self.myWidget.val2.text())
-        v1 = 1
-        v2 = 4
-        print("Sum of "+str(v1)+" and "+str(v2)+": "+str(v1+v2))
-        """
+        # Add plugins one by one into the QListWidget "pluginlist".
+        # for i in range(len(data1)):
+        for i, plugin_name in enumerate(data1):
+            self.myWidget.pluginlist.addItem(plugin_name)
 
 if __name__ == '__main__':
     import sys
@@ -55,5 +62,6 @@ if __name__ == '__main__':
     win = MyWidget()
     win.show()
 
-    app.connect(app, QtCore.SIGNAL("lastWindowClosed()"), app, QtCore.SLOT("quit()"))
+    app.connect(app, QtCore.SIGNAL("lastWindowClosed()"), app,
+                QtCore.SLOT("quit()"))
     app.exec_()
